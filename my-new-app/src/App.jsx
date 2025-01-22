c76c7f7a2c9cd792dc78c671741581f6563c253d
okeke chidera <okekechidera293@gmail.com>
2025-01-22 16:10:43 +0100

    Initial commit

---
+++ my-new-app/src/App.jsx
@@ -0,0 +1,25 @@
+
+function App() {
+  return (
+    <>
+      <div>
+      <img src="/public/image10.png" alt="React Image" />
+        <p>ReactFacts</p>
+      </div>
+
+      <div>
+        <ul>
+          <h1>Fun facts about React</h1>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 200K stars on GitHub</li>
+          <li>Was first released in 2013</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ my-new-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Your main component
+import './index.css'; // Optional CSS file
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
